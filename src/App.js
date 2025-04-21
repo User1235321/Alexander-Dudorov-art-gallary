837898e9739d2e4d08a0d5afe7e5fcad00aa80eb
Vladislav Chistyakov <128889342+jiggerman@users.noreply.github.com>
2025-04-21 17:16:58 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import Gallery from './components/Gallery/Gallery';
+import ContactForm from './components/ContactForm/ContactForm';
+import Footer from './components/Footer/Footer';
+import './styles/global.css';
 
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+      <main>
+        <Gallery />
+        <ContactForm />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
